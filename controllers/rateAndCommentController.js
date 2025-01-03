c5f94eca46e1d7309cb5cb428e61b539516c42d2
swingsofwindy <22521717@gm.uit.edu.vn>
2025-01-03 23:06:29 +0700

    ban firebase

--- controllers/rateAndCommentController.js
+++ controllers/rateAndCommentController.js
@@ -3,29 +3,47 @@ const {db}=require('../firebase')
 const getRate=async(req, res)=>{
     const songId=req.params.id;
     try {
-        const racSnapshot=await db.collection('rac').where('songId','==',songId).get();
-        const racCount=racSnapshot.size;
-        if (racSnapshot.empty) {
-            return res.status(200).json({
-                racCount: 0,
-                racList: [],
-                message: "No matching documents found."
-            });
-        }
+        // const racSnapshot=await db.collection('rac').where('songId','==',songId).get();
+        // const racCount=racSnapshot.size;
+        // if (racSnapshot.empty) {
+        //     return res.status(200).json({
+        //         racCount: 0,
+        //         racList: [],
+        //         message: "No matching documents found."
+        //     });
+        // }
 
-        const racList=racSnapshot.docs.map(doc=>{
-            const data=doc.data();
-            return{
-                creatorId: data.creatorId,
-                rate: data.rate,
-                comment:data.comment
-            };
-        });
+        // const racList=racSnapshot.docs.map(doc=>{
+        //     const data=doc.data();
+        //     return{
+        //         creatorId: data.creatorId,
+        //         rate: data.rate,
+        //         comment:data.comment
+        //     };
+        // });
 
+        // res.status(201).json({
+        //     racCount:racCount,
+        //     racList:racList
+        // });
         res.status(201).json({
-            racCount:racCount,
-            racList:racList
+            racCount:5,
+            racList:[
+                {
+                    creatorId: 'YenTran',
+                    rate: 5,
+                    comment: 'Chill.'
+                },
+                {
+                    creatorId:'KhoaQuan',
+                    rate: 4,
+                    comment: 'Sleep.'
+                }
+            ]
         });
+        console.log({
+            songId: songId
+        })
     } catch (error) {
         res.status(400).json({message: "Fail", error:error.message});
     }
@@ -36,14 +54,20 @@ const addRate=async(req, res)=>{
     const songId=req.params.id;
     const{uid,rate, comment}=req.body;
     try {
-        await db.collection('rac').doc().set({
+        // await db.collection('rac').doc().set({
+        //     songId: songId,
+        //     creatorId:uid,
+        //     rate: rate||'0',
+        //     comment: comment
+        // });
+        // res.status(201).json({
+        //     message:"Success."
+        // })
+        console.log({
             songId: songId,
-            creatorId:uid,
-            rate: rate||'0',
+            uid: uid,
+            rate: rate,
             comment: comment
-        });
-        res.status(201).json({
-            message:"Success."
         })
     } catch (error) {
         res.status(400).json({message: "Fail.", error:error.message});
