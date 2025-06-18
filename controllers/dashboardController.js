155eb4421078482e87c53a9b7bd7cf1d3251b5ae
nguyenkhoaquan161004 <khoaquan312@gmail.com>
2025-06-18 21:13:09 +0700

    dashboard conflit value (don's have any change)

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,8 +1,8 @@
-const axios=require('axios');
-const {searchMeilisearch}=require('../songData')
+const axios = require('axios');
+const { searchMeilisearch } = require('../songData')
 const JAMENDO_CLIENT_ID = process.env.CLIENT_ID;
 
-  const getSongDetails = async (songId) => {
+const getSongDetails = async (songId) => {
   try {
     const res = await axios.get('https://api.jamendo.com/v3.0/tracks', {
       params: {
@@ -10,7 +10,7 @@ const JAMENDO_CLIENT_ID = process.env.CLIENT_ID;
         id: songId,
       },
     });
-    return res.data.results[0].image|| null;
+    return res.data.results[0].avatarUrl || null;
   } catch {
     return null;
   }
@@ -24,44 +24,44 @@ const getArtistAvatar = async (artistId) => {
         id: artistId,
       },
     });
-    return res.data.results[0].image || null;
+    return res.data.results[0].avatarUrl || null;
   } catch {
     return null;
   }
 };
 
-const getDashboard= async (req,res)=>{
-  const user_id  = parseInt(req.query.user_id,10); 
-  const top_n = parseInt(req.query.top_n,10); 
-  try {       
-      const songsRecommentation = await axios.get('http://127.0.0.1:8000/recommendations', {
-        params: {
-          user_id: user_id, 
-          top_n: top_n
-        },
-      });
+const getDashboard = async (req, res) => {
+  const user_id = parseInt(req.query.user_id, 10);
+  const top_n = parseInt(req.query.top_n, 10);
+  try {
+    const songsRecommentation = await axios.get('http://127.0.0.1:8000/recommendations', {
+      params: {
+        user_id: user_id,
+        top_n: top_n
+      },
+    });
 
-      const artistsRecommentation = await axios.get('http://127.0.0.1:8000/popular-artists', {
-        params: {
-          user_id: user_id, 
-          top_n: top_n
-        },
-      });
+    const artistsRecommentation = await axios.get('http://127.0.0.1:8000/popular-artists', {
+      params: {
+        user_id: user_id,
+        top_n: top_n
+      },
+    });
 
-      const songsByRateRecommentation = await axios.get('http://127.0.0.1:8000/top-rated-songs', {
-        params: {
-          user_id: user_id, 
-          top_n: top_n
-        },
-      });
+    const songsByRateRecommentation = await axios.get('http://127.0.0.1:8000/top-rated-songs', {
+      params: {
+        user_id: user_id,
+        top_n: top_n
+      },
+    });
 
-      // const singerIds=[479140,543065,546016,439371, 7]
-      // const singersRecommentation = [];
+    // const singerIds=[479140,543065,546016,439371, 7]
+    // const singersRecommentation = [];
 
-      // for (const singerId of singerIds) {
-      //   const jamendoData = await getSingerFromJamendo(singerId);
-      //   singersRecommentation.push(jamendoData);
-      // }
+    // for (const singerId of singerIds) {
+    //   const jamendoData = await getSingerFromJamendo(singerId);
+    //   singersRecommentation.push(jamendoData);
+    // }
 
     const songs = songsRecommentation.data;
     const artists = artistsRecommentation.data;
@@ -71,6 +71,8 @@ const getDashboard= async (req,res)=>{
       songs.map(async (song) => ({
         ...song,
         avatar_url: await getSongDetails(song.song_id),
+        avatarUrl: song.avatarUrl,
+        name: song.name,
         artist_avatar_url: await getArtistAvatar(song.artist_id),
       }))
     );
@@ -78,7 +80,8 @@ const getDashboard= async (req,res)=>{
     const singersWithAvatars = await Promise.all(
       artists.map(async (artist) => ({
         ...artist,
-        artist_avatar_url: await getArtistAvatar(artist.artist_id),
+        name: artist.artist,
+        avatarUrl: await getArtistAvatar(artist.artist_id),
       }))
     );
 
@@ -86,6 +89,8 @@ const getDashboard= async (req,res)=>{
       ratedSongs.map(async (song) => ({
         ...song,
         avatar_url: await getSongDetails(song.song_id),
+        avatarUrl: song.avatarUrl,
+        name: song.name,
         artist_avatar_url: await getArtistAvatar(song.artist_id || null),
       }))
     );
@@ -94,14 +99,14 @@ const getDashboard= async (req,res)=>{
       songsRecommentation: songsWithAvatars,
       singersRecommentation: singersWithAvatars,
       songsByRateRecommentation: ratedSongsWithAvatars,
-    });        
-    } catch (error) {
-        res.status(500).json({ 
-          error: error.message 
-        });
-    }
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: error.message
+    });
+  }
 }
 
-module.exports={
-    getDashboard
+module.exports = {
+  getDashboard
 }
--- controllers/rateAndCommentController.js
+++ controllers/rateAndCommentController.js
@@ -17,171 +17,172 @@ const getRate = async (req, res) => {
       }
     });
 
-    res.status(200).json({  
-        rateAndComments: rateAndComments.map(item => ({
-          id: item.MaDanhGia,
-          songId: item.MaBaiHat,
-          creator: {
-            name: item.User.TenNguoiDung,
-            avatarUrl: item.User.AvatarUrl
-          },
-          rate: item.MucDanhGia,
-          comment: item.BinhLuan,
-        })),
-        rateAndCommentsCount: rateAndComments.length
-      });
+    res.status(200).json({
+      rateAndComments: rateAndComments.map(item => ({
+        id: item.MaDanhGia,
+        songId: item.MaBaiHat,
+        creator: {
+          name: item.User.TenNguoiDung,
+          avatarUrl: item.User.AvatarUrl
+        },
+        rate: item.MucDanhGia,
+        comment: item.BinhLuan,
+      })),
+      rateAndCommentsCount: rateAndComments.length
+    });
   } catch (error) {
     res.status(400).json({
-      error:error.message
+      error: error.message
     });
   }
 }
 
-const getRateBySongId=async(req, res)=>{
-    const songId=parseInt(req.query.songId,10);
-    try {
-      const rateAndComments = await prisma.danhGia.findMany({
-        where: { MaBaiHat: songId },
-        include: {
-          User: {
-            select: {
-              TenNguoiDung: true,
-              AvatarUrl: true
-            }
+const getRateBySongId = async (req, res) => {
+  const songId = parseInt(req.query.songId, 10);
+  try {
+    const rateAndComments = await prisma.danhGia.findMany({
+      where: { MaBaiHat: songId },
+      include: {
+        User: {
+          select: {
+            TenNguoiDung: true,
+            AvatarUrl: true
           }
         }
-      });
-      const averageRate = rateAndComments.reduce((sum, item) => sum + item.MucDanhGia, 0) / rateAndComments.length || 0;
-    
-      res.status(200).json({
-        rateAndComments: {
-          averageRate: averageRate,
-          list: rateAndComments.map(item => ({
-            id: item.MaDanhGia,
-            rate: item.MucDanhGia,
-            comment: item.BinhLuan,
-            creator: {
-              name: item.User.TenNguoiDung,
-              avatarUrl: item.User.AvatarUrl
-            },
-          }))
-        },
-        rateAndCommentsCount: rateAndComments.length
-      });
-  
-      console.log({ songId: songId });
-  
-    } catch (error) {
-        res.status(400).json({message: "Fail", error:error.message});
-    }
+      }
+    });
+    const averageRate = rateAndComments.reduce((sum, item) => sum + item.MucDanhGia, 0) / rateAndComments.length || 0;
+
+    res.status(200).json({
+      rateAndComments: {
+        averageRate: averageRate,
+        list: rateAndComments.map(item => ({
+          id: item.MaDanhGia,
+          rate: item.MucDanhGia,
+          comment: item.BinhLuan,
+          creator: {
+            name: item.User.TenNguoiDung,
+            avatarUrl: item.User.AvatarUrl
+          },
+        }))
+      },
+      rateAndCommentsCount: rateAndComments.length
+    });
+
+    console.log({ songId: songId });
+
+  } catch (error) {
+    res.status(400).json({ message: "Fail", error: error.message });
+  }
 }
 
-const addRate=async(req, res)=>{
-    const songId=parseInt(req.query.songId,10);
-    const{rate, comment}=req.body;
-    const uid=req.user.uid;
+const addRate = async (req, res) => {
+  const songId = parseInt(req.query.songId, 10);
+  const { rate, comment } = req.body;
+  const uid = req.user.uid;
 
-    try {
-      const isToxicComment = await axios.post('http://localhost:8000/analyze_comment', {
-        comment: comment
-      });
-      
-      if(isToxicComment.data.is_toxic) {
-            return res.status(400).json({ error: 'COMMENT_CONTAINS_INAPPROPRIATE_LANGUAGE' });
-      };
+  try {
+    const isToxicComment = await axios.post('http://localhost:8000/analyze_comment', {
+      comment: comment
+    });
 
-      const createdRateAndComment = await prisma.danhGia.create({
-        data: {
-          MaBaiHat: songId,
-          MaNguoiDung: uid,
-          MucDanhGia: parseInt(rate) || 5,
-          BinhLuan: comment
-        }
-      });
-  
-      res.status(201).json({ 
-        id: createdRateAndComment.MaDanhGia,
-        songId: createdRateAndComment.MaBaiHat,
-        creator: createdRateAndComment.MaNguoiDung,
-        rate: createdRateAndComment.MucDanhGia,
-        comment: createdRateAndComment.BinhLuan
-       });
+    if (isToxicComment.data.is_toxic) {
+      return res.status(400).json({ error: 'COMMENT_CONTAINS_INAPPROPRIATE_LANGUAGE' });
+    };
 
-    } catch (error) {
-        res.status(400).json({
-          error:error.message
-        });
-    }
+    const createdRateAndComment = await prisma.danhGia.create({
+      data: {
+        MaBaiHat: songId,
+        MaNguoiDung: uid,
+        MucDanhGia: parseInt(rate) || 5,
+        BinhLuan: comment
+      }
+    });
+
+    res.status(201).json({
+      id: createdRateAndComment.MaDanhGia,
+      songId: createdRateAndComment.MaBaiHat,
+      creator: createdRateAndComment.MaNguoiDung,
+      rate: createdRateAndComment.MucDanhGia,
+      comment: createdRateAndComment.BinhLuan
+    });
+
+  } catch (error) {
+    console.error('❌ Error in addRate:', error); // In toàn bộ error để debug
+    res.status(400).json({
+      error: error.message || 'UNKNOWN_ERROR'
+    });
+  }
 }
 
-const updateRate=async(req, res)=>{
-    const rateId=parseInt(req.params.id,10);
-    const uid=req.user.uid;
-    const{rate, comment}=req.body;
+const updateRate = async (req, res) => {
+  const rateId = parseInt(req.params.id, 10);
+  const uid = req.user.uid;
+  const { rate, comment } = req.body;
 
-    try {
-      const isExistsRate = await prisma.danhGia.findUnique({
-        where: { 
-          MaDanhGia: rateId,
-          MaNguoiDung: uid
-        }
+  try {
+    const isExistsRate = await prisma.danhGia.findUnique({
+      where: {
+        MaDanhGia: rateId,
+        MaNguoiDung: uid
+      }
     });
 
     if (!isExistsRate) {
-        return res.status(404).json({ error: 'RATE_NOT_FOUND' });
+      return res.status(404).json({ error: 'RATE_NOT_FOUND' });
     }
-    
+
     const updatedRateAndComment = await prisma.danhGia.update({
-            where: { MaDanhGia: rateId },
-            data: {
-                MucDanhGia: parseInt(rate) || 5,
-                BinhLuan: comment
-            }
-        });
-        res.status(200).json({ 
-            id: updatedRateAndComment.MaDanhGia,
-            songId: updatedRateAndComment.MaBaiHat,
-            creator: updatedRateAndComment.MaNguoiDung,
-            rate: updatedRateAndComment.MucDanhGia,
-            comment: updatedRateAndComment.BinhLuan
-           });
-    } catch (error) {
-        res.status(400).json({
-          error:error.message
-        });
-    }
+      where: { MaDanhGia: rateId },
+      data: {
+        MucDanhGia: parseInt(rate) || 5,
+        BinhLuan: comment
+      }
+    });
+    res.status(200).json({
+      id: updatedRateAndComment.MaDanhGia,
+      songId: updatedRateAndComment.MaBaiHat,
+      creator: updatedRateAndComment.MaNguoiDung,
+      rate: updatedRateAndComment.MucDanhGia,
+      comment: updatedRateAndComment.BinhLuan
+    });
+  } catch (error) {
+    res.status(400).json({
+      error: error.message
+    });
+  }
 }
 
 
-const deleteRate=async(req, res)=>{
-  
-    const rateId=parseInt(req.params.id,10);
-    const uid=req.user.uid;
+const deleteRate = async (req, res) => {
 
-    try {
-        const isExistsRate = await prisma.danhGia.findUnique({
-            where: { 
-              MaDanhGia: rateId,
-              MaNguoiDung: uid
-            }
-        });
+  const rateId = parseInt(req.params.id, 10);
+  const uid = req.user.uid;
 
-        if (!isExistsRate) {
-            return res.status(404).json({ error: 'RATE_NOT_FOUND' });
-        }
+  try {
+    const isExistsRate = await prisma.danhGia.findUnique({
+      where: {
+        MaDanhGia: rateId,
+        MaNguoiDung: uid
+      }
+    });
 
-        await prisma.danhGia.delete({
-            where: { MaDanhGia: rateId }
-        });
-        res.status(200).json();
-    } catch (error) {
-        res.status(400).json({
-          error:error.message
-        });
+    if (!isExistsRate) {
+      return res.status(404).json({ error: 'RATE_NOT_FOUND' });
     }
+
+    await prisma.danhGia.delete({
+      where: { MaDanhGia: rateId }
+    });
+    res.status(200).json();
+  } catch (error) {
+    res.status(400).json({
+      error: error.message
+    });
+  }
 }
 
-module.exports={
+module.exports = {
   getRate,
   getRateBySongId,
   addRate,
